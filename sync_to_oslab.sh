7c9075147a05ca5a521b0c86f7348fd2ad072025
jiangzhang <jiangzhangsg@qq.com>
2021-03-18 19:58:06 +0800

    add sync_to_oslab.sh to sync to oslab

---
+++ sync_to_oslab.sh
@@ -0,0 +1,8 @@
+##########################################################################
+# File Name: sync_to_oslab.sh
+# Author: jiangzhang
+# mail: jiangzhangsg@qq.com
+# Created Time: Thu 18 Mar 2021 19:56:35 +08
+#########################################################################
+#!/bin/zsh
+scp -r _book darfux@lab000.darfux.cc:/var/www/oslab/
